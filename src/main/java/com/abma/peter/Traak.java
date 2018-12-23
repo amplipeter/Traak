b4cb05e9560d554ba521872c61812739e4e7ee77
Peter Abma <charlatans@chello.nl>
2018-12-23 16:16:58 +0100

    nog steeds

---
+++ src/main/java/com/abma/peter/Traak.java
@@ -0,0 +1,24 @@
+package com.abma.peter;
+//import org.slf4j.*;
+
+import org.apache.log4j.BasicConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello worldbbbb!
+ *
+ */
+public class Traak 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Oss my vrld!" );
+        BasicConfigurator.configure();
+		Logger logger = LoggerFactory.getLogger(Traak.class);
+		logger.info("Ongelofelijk dit allemaal !!!!!!");
+		 System.out.println( "Hello nog steeds zeer verbaasd" );
+		 logger.error("Ongelofelijk mmmdit allemaal kk222222");
+		 
+    }
+}
